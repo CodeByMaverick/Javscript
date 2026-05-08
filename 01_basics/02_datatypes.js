365f40a2855b13df6deafcc28d75ad041d6cdc99
Akshant <codebymaverick@gmail.com>
2026-05-08 18:29:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // use all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs not browser
+
+console.log(3
+    + 
+    3); // code readability should be high
+
+console.log("Hello")
+
+
+let name = "Maverick"
+let age = 19
+let isLoggedIn = true
+
+
+// number => 2 to the power 53
+// bigint => for very big numbers 
+// string => 'srting' = can also be used , but preferred = "string"
+// boolean => true/false
+// null => standalone value (declared null)
+// undefined => variable defined but value not assigned 
+// symbol => unique 
+
+console.log(typeof true)
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined
