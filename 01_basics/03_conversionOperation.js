827c9ffd40faa5150b2ee259f3cfe7b4dca58c72
Akshant <codebymaverick@gmail.com>
2026-05-10 18:34:43 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,47 @@ let someNumber = 44
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber); 
+
+
+// ****************************************** Operations ******************************************
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Mate"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// if string first then every element should be treated like a string
+// but if string is last then first conversion will happen 
+
+console.log( (2 + 5) * 4 % 8);
+
+// good for problem solving but in irl write simple understanable code don't make it complex (use parenthesis)
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 
+
+// readability is top priority 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
