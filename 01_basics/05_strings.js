fd51ac5f66bd9a8e98f7c8003b0a3d2965777088
Akshant <codebymaverick@gmail.com>
2026-05-15 17:55:52 +0000

    update in strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -6,7 +6,7 @@ console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('userOne')
+const gameName = new String('user-One-com')
 
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -18,3 +18,18 @@ console.log(gameName.indexOf('n'));
 
 const newString = gameName.substring(3, 6)
 console.log(newString);
+
+const anotherString = gameName.slice(-8, 1)
+console.log(anotherString);
+
+const newStringOne = "     user@gmail.com     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://maverick.com/maverick%20dev"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('maverick'));
+console.log(url.includes('userOne'));
+
+console.log(gameName.split('-'));
