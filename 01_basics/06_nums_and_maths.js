cc36032f21f9b73f4627ee7d7febb7970cee57d9
Akshant <codebymaverick@gmail.com>
2026-05-20 18:32:06 +0000

    nums and maths update

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -7,3 +7,26 @@ console.log(balance);
 console.log(balance.toString());
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
+
+
+const anotherNumber = 145.672
+
+console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++ 
+
+console.log(Math);
+
+console.log(Math.abs(-7));
+console.log(Math.round(8.6));
+
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+
+console.log(Math.min(4, 9, 8, 2));
+console.log(Math.max(4, 9, 8, 2));
