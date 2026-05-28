d27389a0a59223c26250f52591ff69b4652ceb29
Akshant <codebymaverick@gmail.com>
2026-05-28 18:24:31 +0000

    arrays in JS

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ["ironman", "hulk", "thor", "spiderman"]
+
+const myArray2 = new Array(1, 2, 3, 4)
+
+console.log(myArray[0]);
+console.log(myHeros[2]);
+
+
+// Array Methods
+
+myArray.push(7)
+myArray.push(9)
+myArray.pop()
+myArray.unshift(9)
+myArray.shift()
+
+console.log(myArray);
