57c2992c67d84b761c54d6eced775850fc3087b0
Akshant <codebymaverick@gmail.com>
2026-05-29 18:30:05 +0000

    update in arrays JS

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -18,3 +18,25 @@ myArray.unshift(9)
 myArray.shift()
 
 console.log(myArray);
+
+console.log(myArray.includes(5));
+console.log(myArray.indexOf(4));
+
+const newArray = myArray.join()
+
+console.log(myArray);
+console.log(newArray);
+
+console.log(typeof newArray);
+
+// slice, splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log("C ", myArray);
+console.log(myn2);
