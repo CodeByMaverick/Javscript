35a5f0ba6838c720c758cdafb158359ef4a3c8bd
Akshant <codebymaverick@gmail.com>
2026-06-02 13:54:17 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,28 @@
+function sayMyName(){
+console.log("M");
+console.log("A");
+console.log("V");
+console.log("E");
+console.log("R");
+console.log("I");
+console.log("C");
+console.log("K");
+}
+sayMyName()
+
+function addTwoNumbers(number1, number2){       // parameters
+    console.log(number1 + number2);
+}
+addTwoNumbers(3, 7)                           // arguments
+addTwoNumbers(3, "7")
+addTwoNumbers(3, "s")
+addTwoNumbers(3, null)
+
+function addnumbers(number1, number2){
+    let result = number1 + number2
+    return result
+    console.log(Something)            // function rule - do nothing after return
+    return number1 + number2         // short method
+}
+let result = addnumbers(4, 9)
+console.log("Result:", result);
