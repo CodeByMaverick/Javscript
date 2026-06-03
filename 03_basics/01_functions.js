c8f15d6a87fa2dafc306bc024f7ecaa6c61c01aa
Akshant <codebymaverick@gmail.com>
2026-06-03 06:10:31 +0000

    JS functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,3 +47,33 @@ function userLoginMessage2(username = "james"){       // If it has a default val
     return `${username} just logged in`
 }
 console.log(userLoginMessage2());                     // but if a value in entered here so it will override
+
+
+
+function calculateCartPrice(val1, val2, ...num1){                // spread operator
+    return num1
+}
+console.log(calculateCartPrice(200, 300, 400, 600 , 800));
+
+const user = {
+    username: "john",
+    price: 499
+}
+
+function totalBill(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+totalBill(user)
+
+totalBill({
+    username: "sam",
+    price: 599
+})
+
+const myArray = [299, 399, 499, 599]
+function getSecondValue(getArray){
+    return getArray[2]
+}
+console.log(getSecondValue(myArray))
+
+console.log(getSecondValue([100, 200, 300, 400]));
