6b201fe2c42b55d13fbb2c31e7b6bcb80c78131b
Akshant <codebymaverick@gmail.com>
2026-06-05 17:29:22 +0000

    scope level and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,8 +12,48 @@ let a = 500
 if(true){                                 // inside if - block scope 
     let a = 10                           // outside it global scope
     const b = 20
-    console.log("Inner:",a);
+    // console.log("Inner:",a);
 }
-console.log(a);
+// console.log(a);
 
 // global scope inside browser console & global scope inside code space run through node - are different
+
+
+
+function one(){
+    const username = "someone"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(webiste);
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "person1"
+    if(username === "person1"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++ interesting +++++++++++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num){
+    return num + 1
+}
+
+// console.log(addTwo(5));           // can't access before funtcion cause it is held by a variable
+const addTwo = function(num){          // also called expression
+    return num + 2
+}
