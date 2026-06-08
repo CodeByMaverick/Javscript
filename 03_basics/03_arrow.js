b792546d5d90b550033ba80aca32b80258ac9a9a
Akshant <codebymaverick@gmail.com>
2026-06-08 17:18:14 +0000

    arrow in JS

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,7 +3,8 @@ const user = {
     price: 2000,
 
     welcomeMessage : function() {
-        console.log(`${this.username}, welcome to the website`);
+        // console.log(`${this.username}, welcome to the website`);
+        // console.log(this);
     }
 }
 
@@ -12,3 +13,36 @@ user.username = "james"
 user.welcomeMessage()
 
 console.log(this);
+
+// inside browser , global object is window object 
+
+function fn(){
+    const username = "someone"
+    console.log(this.username);
+}
+
+fn()
+
+const fn2 = () => {
+    const username = "someone"
+    console.log(this.username);
+}
+
+fn2()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(4, 6));
+
+
+// const add2 = (num1, num2) => num1 + num2        -  implicit return
+
+// const add2 = (num1, num2) => (num1 + num2)      
+
+const add2 = (num1, num2) => ({username: "personA"})
+
+console.log(add2(7, 7));
+
+// if use {} then return should be present , else no need 
+
